d54912ddea98d676c13db6222e9589cda5022995
Godric Gryffindor <112949204+elitesuper@users.noreply.github.com>
2022-12-21 18:25:30 -0800

    tailwind integrate!

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgdark:"#1e1e1e",
+        bgdarkdrop:"#0e0e0e",
+        bgdarkactivedrop:"#1d1d1d",
+      },
+    },
+  },
+  plugins: [],
+}
